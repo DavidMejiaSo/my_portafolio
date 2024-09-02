008cb2e1137cfbd3aed60a49821849b488b88731
DavidMejiaSo <davidmejiasoto97@gmail.com>
2024-09-02 16:03:24 -0500

    UPDATE: Portafolio web, fixed VSC modal open

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,45 +1,53 @@
+// scripts.js
+
+// Function to show the desktop after BIOS
 function showDesktop() {
     document.getElementById('bios').style.display = 'none';
     document.getElementById('desktop').style.opacity = '1';
 }
 
+// Function to update the clock
 function updateClock() {
     const now = new Date();
     const time = now.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
     document.getElementById('clock').textContent = time;
 }
 
+// Function to open the projects window
 function openProjectsWindow() {
     document.getElementById('projects-window').style.display = 'flex';
 }
 
+// Function to close the projects window
 function closeProjectsWindow() {
     document.getElementById('projects-window').style.display = 'none';
 }
 
+// Function to toggle the start menu
 function toggleStartMenu() {
     const startMenu = document.getElementById('startMenu');
     startMenu.classList.toggle('active');
 }
 
-function openResumeModal() {
-    document.getElementById('resume-frame').src = 'vsc.html'; // Ajusta la ruta si es necesario
-    document.getElementById('resume-modal').classList.remove('hidden');
+// Function to open the GitHub modal
+function openVSCModal() {
+    document.getElementById('vsc-modal').style.display = 'block';
 }
 
-function closeModal() {
-    document.getElementById('resume-modal').classList.add('hidden');
-    document.getElementById('resume-frame').src = ''; // Limpia el src del iframe para evitar problemas
+// Función para cerrar el modal de VSC
+function closeVSCModal() {
+    document.getElementById('vsc-modal').style.display = 'none';
 }
-
-document.querySelectorAll('.icon').forEach(icon => {
-    if (icon.querySelector('span').textContent === 'Résumé') {
-        icon.addEventListener('click', openResumeModal);
-    }
+document.querySelectorAll('.vsc-menubar span, .vsc-file').forEach(item => {
+    item.addEventListener('mouseover', function() {
+        this.style.backgroundColor = '#37373d';
+    });
+    item.addEventListener('mouseout', function() {
+        this.style.backgroundColor = '';
+    });
 });
-
-
 // Initial setup
 setTimeout(showDesktop, 2000);
 setInterval(updateClock, 1000);
 updateClock();
+document.querySelector('.icon:nth-child(2)').onclick = openVSCModal;
